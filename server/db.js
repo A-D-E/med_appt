32b39e5df1b6847c0606558aaf47671a760e0fc7
A-D-E <a.user.de@gmail.com>
2024-04-01 10:09:14 -0400

    some fixes

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:ODI2NC1hdXNlcmRl@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MTU3MzItYXVzZXJk@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
